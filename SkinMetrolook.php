2ad64babf0d3fbe1a6c6022f7cf7ab8affc0a70b
paladox2015 <paladox2015@users.noreply.github.com>
2014-07-20 12:44:54 +0100

    Create SkinMetrolook.php

---
+++ SkinMetrolook.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Vector - Modern version of MonoBook with fresh look and many usability
+ * improvements.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @ingroup Skins
+ */
+
+/**
+ * SkinTemplate class for Vector skin
+ * @ingroup Skins
+ */
+class SkinMetrolook extends SkinTemplate {
+/*	public $skinname = 'vector';
+	public $stylename = 'Vector';
+	public $template = 'VectorTemplate';
+*/
+	var $skinname = 'metrolook', $stylename = 'Metrolook',
+		$template = 'MetrolookTemplate', $useHeadElement = true;
+
+	/**
+	 * Initializes output page and sets up skin-specific parameters
+	 * @param OutputPage $out Object to initialize
+	 */
+	public function initPage( OutputPage $out ) {
+		global $wgLocalStylePath;
+
+		parent::initPage( $out );
+
+		// Append CSS which includes IE only behavior fixes for hover support -
+		// this is better than including this in a CSS file since it doesn't
+		// wait for the CSS file to load before fetching the HTC file.
+		$min = $this->getRequest()->getFuzzyBool( 'debug' ) ? '' : '.min';
+		$out->addHeadItem( 'csshover',
+			'<!--[if lt IE 7]><style type="text/css">body{behavior:url("' .
+				htmlspecialchars( $wgLocalStylePath ) .
+				"/{$this->stylename}/csshover{$min}.htc\")}</style><![endif]-->"
+		);
+
+		$out->addModules( array( 'skins.metrolook.js' ) );
+	}
+
+	/**
+	 * Loads skin and user CSS files.
+	 * @param OutputPage $out
+	 */
+	function setupSkinUserCss( OutputPage $out ) {
+		parent::setupSkinUserCss( $out );
+
+		$styles = array( 'mediawiki.skinning.interface', 'skins.metrolook.styles' );
+		wfRunHooks( 'SkinMetrolookStyleModules', array( $this, &$styles ) );
+		$out->addModuleStyles( $styles );
+	}
+}
