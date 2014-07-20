63d507ad5c5bfdf2421f0c5b4744b39964695ffc
paladox2015 <paladox2015@users.noreply.github.com>
2014-07-20 11:10:31 +0100

    Update Metrolook.php
    Updating Fix for compatibility with mediawiki 1.24 but still errors.

--- Metrolook.php
+++ Metrolook.php
@@ -11,13 +11,15 @@ if( !defined( 'MEDIAWIKI' ) ) {
 	die( -1 );
 }
 
+$wgValidSkinNames['metrolook'] = 'Metrolook';
+
 /**
  * SkinTemplate class for Vector skin
  * @ingroup Skins
  */
 class SkinMetrolook extends SkinTemplate {
 
-	var $skinname = 'metrolook', $stylename = 'metrolook',
+	var $skinname = 'Metrolook', $stylename = 'metrolook',
 		$template = 'MetrolookTemplate', $useHeadElement = true;
 
 	/**
@@ -197,21 +199,62 @@ $(document).click(function(e) {
         toggleDiv(openDiv);
     }
 });
+$(function () {
+  $('.usermenu > div').toggleClass('no-js js');
+  $('.usermenu .js div').hide();
+  $('.usermenu .js').click(function(e) {
+    $('.usermenu .js div').fadeToggle(150);
+    $('.usermenu').toggleClass('active');
+    e.stopPropagation();
+  });
+  $(document).click(function() {
+    if ($('.usermenu .js div').is(':visible')) {
+      $('.usermenu .js div', this).fadeOut(150);
+      $('.usermenu').removeClass('active');
+    }
+  });
+});
+
+$(function () {
+  $('.actionmenu > div').toggleClass('no-js js');
+  $('.actionmenu .js div').hide();
+  $('.actionmenu .js').click(function(e) {
+    $('.actionmenu .js div').fadeToggle(150);
+    $('.clicker').toggleClass('active');
+    e.stopPropagation();
+  });
+  $(document).click(function() {
+    if ($('.actionmenu .js div').is(':visible')) {
+      $('.actionmenu .js div', this).fadeOut(150);
+      $('.clicker').removeClass('active');
+    }
+  });
+});
     </script>
-<script src="http://files.pidgi.net/snowstorm/snowstorm.js"></script>
+<link href='http://fonts.googleapis.com/css?family=Yanone+Kaffeesatz:700' defer="defer" rel='stylesheet' type='text/css'>
 <meta name="msapplication-TileImage" content="http://www.pidgi.net/new/public/images/pidgiwiki.png"/>
-<meta name="msapplication-TileColor" content="#CF8B54"/>
+<meta name="msapplication-TileColor" content="#BE0027"/>
+<script src="http://files.pidgi.net/overthrow.js"></script>
 		<div id="mw-page-base" class="noprint"></div>
 		<div id="mw-head-base" class="noprint"></div>
-			<?php if( $this->data['newtalk'] ): ?>
-			<!-- newtalk -->
-			<div class="usermessage"><?php $this->html( 'newtalk' )  ?></div>
-			<!-- /newtalk -->
-			<?php endif; ?>
 		<!-- content -->
-		<div id="content">
+		<div id="content" class="overthrow">
 			<a id="top"></a>
 			<div id="mw-js-message" style="display:none;"<?php $this->html( 'userlangattributes' ) ?>></div>
+				<?php if( $this->data['newtalk'] ): ?>
+				<!-- newtalk -->
+				<div class="usermessage"><?php $this->html( 'newtalk' )  ?></div>
+				<!-- /newtalk -->
+				<?php endif; ?>
+<div style="text-align:center;" align="center"><script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Sitenotice -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:728px;height:90px"
+     data-ad-client="ca-pub-9225557705415007"
+     data-ad-slot="2149272158"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script></div>
 			<?php if ( $this->data['sitenotice'] ): ?>
 			<!-- sitenotice -->
 			<div id="siteNotice"><?php $this->html( 'sitenotice' ) ?></div>
@@ -253,6 +296,31 @@ $(document).click(function(e) {
 				<?php $this->html( 'catlinks' ); ?>
 				<!-- /catlinks -->
 				<?php endif; ?>
+<br clear="all" />
+<!-- footer -->
+		<div id="footer"<?php $this->html( 'userlangattributes' ) ?>>
+<hr />
+			<?php foreach( $this->getFooterLinks() as $category => $links ): ?>
+				<ul id="footer-<?php echo $category ?>">
+					<?php foreach( $links as $link ): ?>
+						<li id="footer-<?php echo $category ?>-<?php echo $link ?>"><?php $this->html( $link ) ?></li>
+					<?php endforeach; ?>
+				</ul>
+			<?php endforeach; ?>
+			<?php $footericons = $this->getFooterIcons("icononly");
+			if ( count( $footericons ) > 0 ): ?>
+				<ul id="footer-icons" class="noprint">
+<?php			foreach ( $footericons as $blockName => $footerIcons ): ?>
+					<li id="footer-<?php echo htmlspecialchars( $blockName ); ?>ico">
+<?php				foreach ( $footerIcons as $icon ): ?>
+						<?php echo $this->skin->makeFooterIcon( $icon ); ?>
+<?php				endforeach; ?>
+					</li>
+<?php			endforeach; ?>
+				</ul>
+			<?php endif; ?>
+		</div>
+		<!-- /footer -->
 				<?php if ( $this->data['dataAfterContent'] ): ?>
 				<!-- dataAfterContent -->
 				<?php $this->html( 'dataAfterContent' ); ?>
@@ -268,30 +336,49 @@ $(document).click(function(e) {
 		<!-- /content -->
 		<!-- header -->
 		<div id="mw-head" class="noprint">
-			<div class="vectorMenu" style="float:right;background-image:none;vertical-align:middle;height:40px;padding-left:12px;padding-right:12px;"><div id="username-top"><?php $this->getSkin()->getUser()->getName(); ?></div><div class="menu" style="position:relative;top:0px;right:-49px;"><?php $this->renderNavigation( 'PERSONAL' ); ?></div></div>
-<div><img src="http://images.pidgi.net/10x40.png" style="float:left;" /><div class="onhoverbg" style="height:40px;float:left;"><a href="http://www.pidgi.net/wiki/Main_Page"><img src="http://images.pidgi.net/pidgiwiki.png" /></a></div><img src="http://images.pidgi.net/line.png" style="float:left;" /><div class="onhoverbg" style="height:40px;float:left;"><img src="http://images.pidgi.net/downarrow.png" style="cursor:pointer;" onclick="toggleDiv('bartile');"></div></div>
+			<div class="vectorMenu usermenu" style="float:right;background-image:none;vertical-align:middle;height:40px;padding-left:10px;padding-right:10px;position:absolute;top:0px;right:10px;width:auto;text-align:right;">
+  <div class="no-js">
+<a href="javascript:void(0);" style="text-decoration:none;"><span id="username-top"><?php
+if ($_SERVER["REMOTE_ADDR"] == htmlspecialchars($this->getSkin()->getUser()->getName())) {
+echo "Guest";
+}
+else {
+echo htmlspecialchars( $this->getSkin()->getUser()->getName() );
+}
+ ?><span style="word-spacing:4px;"> </span><img style="position:relative;top:-1px;" src="<?php
+$default = 'http://www.pidgi.net/wiki/skins/metrolook/images/user-icon.png';
+$grav_url = 'http://www.gravatar.com/avatar/' . md5( strtolower( trim( $this->getSkin()->getUser()->getEmail() ) ) ) . '?d=' . urlencode ( $default ) . '&s=' . 20;
+echo $grav_url;
+?>" /></span></a>
+<div class="menu" style="position:absolute;top:40px;right:0px;margin:0;width:200px;">
+<?php $this->renderNavigation( 'PERSONAL' ); ?>
+</div>
+</div>
+</div>
+<div style="padding-left:10px;"><div class="lighthover" style="height:40px;float:left;"><div class="onhoverbg" style="height:40px;float:left;"><a href="http://www.pidgi.net/wiki/Main_Page"><img src="http://images.pidgi.net/pidgiwiki.png" /></a></div><img src="http://images.pidgi.net/line.png" style="float:left;" /><div class="onhoverbg" style="height:40px;float:left;"><img src="http://images.pidgi.net/downarrow.png" style="cursor:pointer;" onclick="toggleDiv('bartile');"></div></div></div>
 	<div id="top-tile-bar" class="fixed-position">
 <div style="vertical-align:top;align:left;">
 <div class="topleft">
 <div style="align:left;margin-left:auto;margin-right:auto;display:none;height:200px;" class="tilebar" id="bartile"><div style="height:200px;display:table;"><div style="vertical-align:middle;display:table-cell;padding-left:36px;">
-<div style="float:left;padding:5px;"><span class="tile"><a href="http://www.pidgi.net/wiki/"><img src="http://images.pidgi.net/pidgiwikitiletop.png" /></a></span></div><div style="float:left;padding:5px;"><span class="tile"><a href="http://www.pidgi.net/press/"><img src="http://images.pidgi.net/pidgipresstiletop.png" /></a></span></div><div style="float:left;padding:5px;"><span class="tile"><a href="http://www.petalburgwoods.com/"><img src="http://images.pidgi.net/pwntiletop.png" /></a></span></div>
+<div style="float:left;padding:5px;"><span class="tile"><a href="http://www.pidgi.net/wiki/"><img src="http://images.pidgi.net/pidgiwikitiletop.png" /></a></span></div><div style="float:left;padding:5px;"><span class="tile"><a href="http://www.pidgi.net/press/"><img src="http://images.pidgi.net/pidgipresstiletop.png" /></a></span></div><div style="float:left;padding:5px;"><span class="tile"><a href="http://www.pidgi.net/jcc/"><img src="http://images.pidgi.net/jcctiletop.png" /></a></span></div><div style="float:left;padding:5px;"><span class="tile"><a href="http://www.petalburgwoods.com/"><img src="http://images.pidgi.net/pwntiletop.png" /></a></span></div>
 
 </div></div></div>
 </div>
 
 </div></div>
 			<div id="left-navigation">
-				<a href="http://www.pidgi.net/wiki/Special:Upload"><div class="onhoverbg" style="padding-left:0.8em;padding-right:0.8em;float:left;height:40px;font-size:10pt;"><img src="http://images.pidgi.net/uploadlogo.png" /> <span style="color:#fff;position:relative;top:1px;">Upload file</span></div></a><?php $this->renderNavigation( array( 'NAMESPACES', 'VARIANTS', 'VIEWS', 'ACTIONS' ) ); ?>
+				<a href="http://www.pidgi.net/wiki/Special:Upload"><div class="onhoverbg" style="padding-left:0.8em;padding-right:0.8em;float:left;height:40px;font-size:10pt;"><img src="http://images.pidgi.net/uploadlogo.png" /> <span style="color:#fff;position:relative;top:1px;">Upload</span></div></a><?php $this->renderNavigation( array( 'NAMESPACES', 'VARIANTS', 'VIEWS', 'ACTIONS' ) ); ?>
 			</div>
 		</div>
 		<!-- /header -->
 		<!-- panel -->
 			<div id="mw-panel" class="noprint">
 				<?php $this->renderNavigation( array( 'SEARCH' ) ); ?>
-				<br style="clear:both;" />
+				<br style="clear:both;line-height:0%;" />
+<a href="http://www.pidgi.net/wiki/PidgiWiki:Requests" style="text-decoration:none;" class="hoverbox"><div style="width:142px;margin-left:10px;margin-top:4px;margin-bottom:4px;padding:8px;text-align:center;color:#fff;font-size:80%;" class="hoverbox">Send in a request</div></a>
 				<?php $this->renderPortals( $this->data['sidebar'] ); ?>
-	<!-- feature -->
-<div class="portal" id='p-Feature'>
+<!-- feature -->
+<div class="portal expanded" id='p-Feature'>
 	<h5>Featured media</h5>
 	<div class="body">
 				<ul style="padding-right:20px;">
@@ -303,56 +390,29 @@ $(document).click(function(e) {
 			</div>
 </div>
 <!-- /feature -->
-<div class="portal" id='p-Ads'>
-	<h5>Advertisements</h5>
+<div id="p-Ads">
 	<div class="body">
 				<ul style="padding-right:20px;">
-<script type="text/javascript"><!--
-google_ad_client = "ca-pub-9225557705415007";
-/* Sidebar2 */
-google_ad_slot = "1558107492";
-google_ad_width = 120;
-google_ad_height = 240;
-//-->
-</script>
-<script type="text/javascript"
-src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+<!-- PidgiWiki left -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:120px;height:240px"
+     data-ad-client="ca-pub-9225557705415007"
+     data-ad-slot="2042690559"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
 </script>
 				</ul>
 			</div>
 </div>
 			</div>
 		<!-- /panel -->
-		<!-- footer -->
-		<div id="footer"<?php $this->html( 'userlangattributes' ) ?>>
-			<?php foreach( $this->getFooterLinks() as $category => $links ): ?>
-				<ul id="footer-<?php echo $category ?>">
-					<?php foreach( $links as $link ): ?>
-						<li id="footer-<?php echo $category ?>-<?php echo $link ?>"><?php $this->html( $link ) ?></li>
-					<?php endforeach; ?>
-				</ul>
-			<?php endforeach; ?>
-			<?php $footericons = $this->getFooterIcons("icononly");
-			if ( count( $footericons ) > 0 ): ?>
-				<ul id="footer-icons" class="noprint">
-<?php			foreach ( $footericons as $blockName => $footerIcons ): ?>
-					<li id="footer-<?php echo htmlspecialchars( $blockName ); ?>ico">
-<?php				foreach ( $footerIcons as $icon ): ?>
-						<?php echo $this->skin->makeFooterIcon( $icon ); ?>
-
-<?php				endforeach; ?>
-					</li>
-<?php			endforeach; ?>
-				</ul>
-			<?php endif; ?>
-			<div style="clear:both"></div>
-		</div>
-		<!-- /footer -->
+			<div id="mw-panel-right" class="noprint">
+				<a href="http://www.cardteam.co.uk/?acc=8e296a067a37563370ded05f5a3bf3ec&bannerid=6" target="_blank"><img src="http://www.cardteam.co.uk/js/magestore/affiliateplus/banner.php?id=6&account_id=26&store_id=1" title="Pokemon (160x600) Wide Skyscraper" width="160" height="600" /></a>
+			</div>
 		<!-- fixalpha -->
 		<script type="<?php $this->text( 'jsmimetype' ) ?>"> if ( window.isMSIE55 ) fixalpha(); </script>
 		<!-- /fixalpha -->
 		<?php $this->printTrail(); ?>
-
 	</body>
 </html>
 <?php
@@ -399,13 +459,30 @@ src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
 		}
 	}
 
-	private function renderPortal( $name, $content, $msg = null, $hook = null ) {
-		if ( !isset( $msg ) ) {
+	/**
+	 * @param string $name
+	 * @param array $content
+	 * @param null|string $msg
+	 * @param null|string|array $hook
+	 */
+	protected function renderPortal( $name, $content, $msg = null, $hook = null ) {
+		if ( $msg === null ) {
 			$msg = $name;
 		}
+		$msgObj = wfMessage( $msg );
 		?>
-<div class="portal" id='<?php echo Sanitizer::escapeId( "p-$name" ) ?>'<?php echo Linker::tooltip( 'p-' . $name ) ?>>
-	<h5<?php $this->html( 'userlangattributes' ) ?>><?php $msgObj = wfMessage( $msg ); echo htmlspecialchars( $msgObj->exists() ? $msgObj->text() : $msg ); ?></h5>
+		<div class="portal" role="navigation" id='<?php
+		echo Sanitizer::escapeId( "p-$name" )
+		?>'<?php
+		echo Linker::tooltip( 'p-' . $name )
+		?> aria-labelledby='<?php echo Sanitizer::escapeId( "p-$name-label" ) ?>'>
+			<h5<?php
+			$this->html( 'userlangattributes' )
+			?> id='<?php
+			echo Sanitizer::escapeId( "p-$name-label" )
+			?>'><?php
+				echo htmlspecialchars( $msgObj->exists() ? $msgObj->text() : $msg );
+				?></h5>
 	<div class="body">
 <?php
 		if ( is_array( $content ) ): ?>
@@ -504,8 +581,9 @@ src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
 				break;
 				case 'ACTIONS':
 ?>
-<div id="p-cactions" class="vectorMenu<?php if ( count( $this->data['action_urls'] ) == 0 ) echo ' emptyPortlet'; ?>">
-	<h5><span><?php $this->msg( 'actions' ) ?></span><a href="#"></a></h5>
+<div id="p-cactions" class="vectorMenu actionmenu<?php if ( count( $this->data['action_urls'] ) == 0 ) echo ' emptyPortlet'; ?>">
+   <div class="no-js">
+	<h5><a href="javascript:void(0);"><span><?php $this->msg( 'actions' ) ?></span></a></h5>
 	<div class="menu">
 		<ul<?php $this->html( 'userlangattributes' ) ?>>
 			<?php foreach ( $this->data['action_urls'] as $link ): ?>
@@ -513,6 +591,7 @@ src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
 			<?php endforeach; ?>
 		</ul>
 	</div>
+   </div>
 </div>
 <?php
 				break;
@@ -552,6 +631,44 @@ src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
 		<?php endif; ?>
 	</form>
 </div>
+<!-- Quantcast Tag -->
+<script type="text/javascript">
+var _qevents = _qevents || [];
+
+(function() {
+var elem = document.createElement('script');
+elem.src = (document.location.protocol == "https:" ? "https://secure" : "http://edge") + ".quantserve.com/quant.js";
+elem.async = true;
+elem.type = "text/javascript";
+var scpt = document.getElementsByTagName('script')[0];
+scpt.parentNode.insertBefore(elem, scpt);
+})();
+
+_qevents.push({
+qacct:"p-UKb2rEmzCK5k3"
+});
+</script>
+
+<noscript>
+<div style="display:none;">
+<img src="//pixel.quantserve.com/pixel/p-UKb2rEmzCK5k3.gif" border="0" height="1" width="1" alt="Quantcast"/>
+</div>
+</noscript>
+<!-- End Quantcast tag -->
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-38107770-1']);
+  _gaq.push(['_setDomainName', 'pidgi.net']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 <?php
 
 				break;
