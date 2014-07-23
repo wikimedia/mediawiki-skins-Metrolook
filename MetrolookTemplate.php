b0281307037730cbb80a4aad8547cc222efd82c7
paladox2015 <paladox2015@users.noreply.github.com>
2014-07-23 11:26:06 +0100

    Update MetrolookTemplate.php
    This patch removes the rest of google analytics codes.

--- MetrolookTemplate.php
+++ MetrolookTemplate.php
@@ -559,44 +559,6 @@ echo $grav_url;
 		<?php endif; ?>
 	</form>
 </div>
-<!-- Quantcast Tag -->
-<script type="text/javascript">
-var _qevents = _qevents || [];
-
-(function() {
-var elem = document.createElement('script');
-elem.src = (document.location.protocol == "https:" ? "https://secure" : "http://edge") + ".quantserve.com/quant.js";
-elem.async = true;
-elem.type = "text/javascript";
-var scpt = document.getElementsByTagName('script')[0];
-scpt.parentNode.insertBefore(elem, scpt);
-})();
-
-_qevents.push({
-qacct:"p-UKb2rEmzCK5k3"
-});
-</script>
-
-<noscript>
-<div style="display:none;">
-<img src="//pixel.quantserve.com/pixel/p-UKb2rEmzCK5k3.gif" border="0" height="1" width="1" alt="Quantcast"/>
-</div>
-</noscript>
-<!-- End Quantcast tag -->
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-38107770-1']);
-  _gaq.push(['_setDomainName', 'pidgi.net']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
 <?php
 
 				break;
