f798ee9a9b206b7fb3226d75237e2b0fcbaa69b9
paladox <thomasmulhall410@yahoo.com>
2014-10-01 00:02:33 +0200

    Update Metrolook
    * Fixes Bugs.
    
    * Please see README.md for information on customizing and options.
    
    Change-Id: Icf01ca798a2437cfde31866ecb712352a8a294ba

--- Metrolook.php
+++ Metrolook.php
@@ -25,7 +25,7 @@ $GLOBALS['wgExtensionCredits']['skin'][] = array(
 	'path' => __FILE__,
 	'name' => 'Metrolook',
 	'description' => 'Metrolook skin for MediaWiki.',
-	'version' => '2.4.4',
+	'version' => '2.4.6',
 	'url' => 'https://www.mediawiki.org/wiki/Skin:Metrolook',
 	'author' => array( 'immewnity', 'paladox2015', 'Craig Davison', 'lagleki' ),
 	'license-name' => 'GPLv2+',
@@ -102,7 +102,6 @@ $GLOBALS['wgResourceModules']['skins.metrolook.styles'] = array(
 	'styles' => array(
 		'screen.less' => array( 'media' => 'screen' ),
 		'screen-hd.less' => array( 'media' => 'screen and (min-width: 982px)' ),
-		'theme.css' => array( 'media' => 'screen' ),
 	),
 	'remoteSkinPath' => 'Metrolook',
 	'localBasePath' => __DIR__,
--- README.md
+++ README.md
@@ -194,6 +194,18 @@ and all you need to do is edit background-colour and the top bar should change c
 	background-image: url('images/page-fade.png');
 }
 
+@media all and (max-width: 1000px) and (min-width: 700px) {
+   #mw-page-base {
+	height: 2.5em;
+	background-color: dodgerBlue;
+	background-position: bottom left;
+	background-repeat: repeat-x;
+	/* This image is only a fallback (for IE 6-9), so we do not @embed it. */
+	background-image: url('images/page-fade.png');
+    min-width:65em;
+}
+}
+
 div.vectorTabs a:hover {
          background-color: blue;
 }
