e57301e37bb626a51536cd2fd118ab37170431ed
paladox <thomasmulhall410@yahoo.com>
2014-10-01 00:02:33 +0200

    Update Metrolook add support for MediaWiki 1.22
    * Fix images issue for good and also give overflow.js a path update in MetrolookTemplate.php.
    
    * And other fixes and changes.
    
    Change-Id: Ic865793aeeafaf54b7484d06aad9b436cdacb0f7

--- Metrolook.php
+++ Metrolook.php
@@ -25,7 +25,7 @@ $wgExtensionCredits['skin'][] = array(
 	'path' => __FILE__,
 	'name' => 'Metrolook',
 	'description' => 'Metrolook skin for MediaWiki.',
-	'version' => '0.3.1',
+	'version' => '0.3.3',
 	'url' => 'https://www.mediawiki.org/wiki/Skin:Metrolook',
 	'author' => array( 'immewnity', 'paladox2015', 'Craig Davison', 'lagleki' ),
 	'license-name' => 'GPLv2+',
@@ -73,8 +73,6 @@ $image6 = false;
 $UploadButton = true;
 
 $logo = false;
-
-$transparent = __DIR__ . '/images/Transparent.gif';
 
 
 // Register modules
