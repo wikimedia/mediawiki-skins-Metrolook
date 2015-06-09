f5808e0c067fb4f6f6719da5e2b20f5a2bb2daa3
paladox <thomasmulhall410@yahoo.com>
2015-06-09 22:19:22 +0200

    Release metrolook 2.6.0 for MediaWiki 1.24
    * Please see changelog for details on changes.
    
    Change-Id: Iccb17b5bc74b89f6e9c9206c3d8b2f5d6b1a2a0b

---
+++ js/metrolook.search.js
@@ -0,0 +1,22 @@
+function isTouchDevice() {
+	return !!('ontouchstart' in window);
+}
+/* This is here to fix js issue with iPad (all models) */
+$(function () {
+	if( isTouchDevice() ) {
+		if( /Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+			$( '#p-search' ).hide();
+			$( 'img.searchbar' ).click(function(e) {
+				$( '#p-search' ).fadeToggle(150);
+				$('.clicker').toggleClass( 'active' );
+				e.stopPropagation();
+			});
+			$( 'img.searchbar').click(function() {
+				if ($( '#p-search' ).is( ':visible' )) {
+					$( '#p-search', this).fadeOut(150);
+					$( '.clicker' ).removeClass( 'active' );
+				}
+			});
+		}
+	}
+});
