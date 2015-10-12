1cbdf81a1dc6f1fdebdf2cebbf1b37bbef6a9d2d
Paladox <thomasmulhall410@yahoo.com>
2015-10-12 15:52:17 +0100

    Update gruntfile.js
    Change-Id: I5197d91dc46e85eeb6103842691700ec20b4b8c8

--- js/metrolook.search.js
+++ js/metrolook.search.js
@@ -1,29 +1,29 @@
 /* global $ */
 
 function isTouchDevice() {
-	return !!('ontouchstart' in window);
+	return !!( 'ontouchstart' in window );
 }
 
 /* This is here to fix js issue with iPad (all models) */
-$(function () {
-	if( isTouchDevice() ) {
-		if( /mobi|alcatel|Android|android|webOS|webos|iPhone|iPod|Wii|Silk|BlackBerry|playstation|phone|nintendo|htc[-_]|IEMobile|CriOS|Opera Mini|opera.m|palm|panasonic|philips|samsung|Mobile|mobile/i.test(navigator.userAgent) ) {
+$( function () {
+	if ( isTouchDevice() ) {
+		if ( /mobi|alcatel|Android|android|webOS|webos|iPhone|iPod|Wii|Silk|BlackBerry|playstation|phone|nintendo|htc[-_]|IEMobile|CriOS|Opera Mini|opera.m|palm|panasonic|philips|samsung|Mobile|mobile/i.test( navigator.userAgent ) ) {
 			$( '#p-search' ).hide();
-			$( 'img.searchbar' ).click(function(e) {
-				$( '#p-search' ).fadeToggle(150);
-				$('.clicker').toggleClass( 'active' );
+			$( 'img.searchbar' ).click( function ( e ) {
+				$( '#p-search' ).fadeToggle( 150 );
+				$( '.clicker' ).toggleClass( 'active' );
 				e.stopPropagation();
-			});
-			$( 'img.searchbar').click(function() {
-				if ($( '#p-search' ).is( ':visible' )) {
-					$( '#p-search', this).fadeOut(150);
+			} );
+			$( 'img.searchbar' ).click( function () {
+				if ( $( '#p-search' ).is( ':visible' ) ) {
+					$( '#p-search', this ).fadeOut( 150 );
 					$( '.clicker' ).removeClass( 'active' );
 				}
-			});
+			} );
 		}
 
 		/* Fix search bar not showing on iPad */
-		if( /kindle|iPad|PlayBook|Tablet/i.test(navigator.userAgent) ) {
+		if( /kindle|iPad|PlayBook|Tablet/i.test( navigator.userAgent ) ) {
 			$( '#p-search' ).show();
 		}
 	}
