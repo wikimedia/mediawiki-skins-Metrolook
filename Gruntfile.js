3e7386221cdd797fa19e7c9c70ea83b2b4129c55
Paladox <thomasmulhall410@yahoo.com>
2016-06-11 22:46:56 +0100

    Update grunt-jsonlint to 1.1.0
    Change-Id: Iad5b7f79391fdd8654d015008c36ff78dc866832

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,6 @@ module.exports = function ( grunt ) {
 	var conf = grunt.file.readJSON( 'skin.json' );
 	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
 	grunt.loadNpmTasks( 'grunt-jsonlint' );
-	grunt.loadNpmTasks( 'grunt-banana-checker' );
 	grunt.loadNpmTasks( 'grunt-jscs' );
 
 	grunt.initConfig( {
@@ -21,16 +20,20 @@ module.exports = function ( grunt ) {
 		jscs: {
 			src: '<%= jshint.all %>'
 		},
-		banana: conf.MessagesDirs,
 		jsonlint: {
+			jshintStyle: {
+				options: {
+					reporter: 'jshint'
+				},
 			all: [
 				'*.json',
 				'**/*.json',
 				'!node_modules/**'
 			]
+			}
 		}
 	} );
 
-	grunt.registerTask( 'test', [ 'jshint', 'jscs', 'jsonlint', 'banana' ] );
+	grunt.registerTask( 'test', [ 'jshint', 'jscs', 'jsonlint' ] );
 	grunt.registerTask( 'default', 'test' );
 };
