724a109cc6951aeb608471bd7fab6c78bd5d2a31
Paladox <thomasmulhall410@yahoo.com>
2017-03-25 16:04:54 +0000

    Add a scroll offset when a table of contents anchor link is clicked
    Patch is by @smkent
    
    Bug: T157532
    Change-Id: I2ef002f7daeb0f215c7b36fa31344090b0ae6db9

---
+++ js/fixed-header-scroll-fix.js
@@ -0,0 +1,17 @@
+/* Add offset when scrolling to an anchor from a table of contents link */
+var href = $( this ).attr( 'href' ),
+	dest = ( $( href ).offset().top -
+		( $( 'div.vectorMenu#usermenu' ).height() + 10 ) );
+	d = ( $( ':target' ).offset().top -
+		( $( 'div.vectorMenu#usermenu' ).height() + 10 ) );
+
+$( '.toc ul a[href^=#]' ).on( 'click', function( e ) {
+	window.history.pushState( {}, '', $( this ).prop( 'href' ) );
+	$( 'html, body' ).scrollTop( dest );
+	e.preventDefault();
+} );
+
+/* Add offset when scrolling to an anchor present at page load time */
+if ( $( ':target' ).length > 0 ) {
+	$( 'html, body' ).scrollTop( d );
+}
