ba1c894c200c677a7dc1b43dbf86733be2609a7c
Paladox <thomasmulhall410@yahoo.com>
2017-03-25 17:53:47 +0000

    Fix  TypeError: undefined is not an object (evaluating '$(href).offset().top')
    Bug: T157532
    Change-Id: I1d94c41656b60169f5cedcf8b067603539a72943

--- js/fixed-header-scroll-fix.js
+++ js/fixed-header-scroll-fix.js
@@ -1,17 +1,31 @@
-/* Add offset when scrolling to an anchor from a table of contents link */
-var href = $( this ).attr( 'href' ),
-	dest = ( $( href ).offset().top -
-		( $( 'div.vectorMenu#usermenu' ).height() + 10 ) ),
-	d = ( $( ':target' ).offset().top -
-		( $( 'div.vectorMenu#usermenu' ).height() + 10 ) );
+jQuery( document ).ready( function( $ ) {
+	/* Add offset when scrolling to an anchor from a table of contents link */
+	var dest,
+		headerHeight = $( 'div.vectorMenu#usermenu' ).height() + 10,
+		// Split location and hash
+		hash;
 
-$( '.toc ul a[href^=#]' ).on( 'click', function( e ) {
-	window.history.pushState( {}, '', $( this ).prop( 'href' ) );
-	$( 'html, body' ).scrollTop( dest );
-	e.preventDefault();
+	$( '.toc ul a[href*="#"]' ).click( function( e ) {
+		try {
+			if ( this && this.href !== '' ) {
+				// Split location and hash
+				hash = this.href.match( /[#].*/g )[ 0 ],
+
+				// Don't reload page if already at same location as last clicked
+				$( 'html, body' ).animate( {
+					scrollTop: $( hash ).offset().top - 41
+				}, 200 );
+			}
+			e.preventDefault();
+			return false;
+		} catch ( e ) { }
+	} );
+
+	/* Add offset when scrolling to an anchor present at page load time */
+	if ( $( ':target' ).length > 0 ) {
+		dest = $( this ).offset().top - headerHeight;
+		$( 'html, body' ).animate( {
+			scrollTop: dest // also use scrollTop to animate scrolling
+		} );
+	}
 } );
-
-/* Add offset when scrolling to an anchor present at page load time */
-if ( $( ':target' ).length > 0 ) {
-	$( 'html, body' ).scrollTop( d );
-}
