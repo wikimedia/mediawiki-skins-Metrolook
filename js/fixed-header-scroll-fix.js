3e91f0d0b451690f1f6a15b184fb4bda5142693a
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2020-01-08 15:21:04 +0000

    build: Updating npm dependencies
    * eslint-config-wikimedia: 0.3.0 → 0.15.0
    * grunt-eslint: 19.0.0 → 22.0.0
    
    Additional changes:
    * Removing manual reportUnusedDisableDirectives for eslint.
    
    Change-Id: I4935d4f811e3b5e02d98017ebbde56708314b8a0

--- js/fixed-header-scroll-fix.js
+++ js/fixed-header-scroll-fix.js
@@ -1,17 +1,17 @@
-jQuery( document ).ready( function( $ ) {
+jQuery( document ).ready( function ( $ ) {
 	/* Add offset when scrolling to an anchor from a table of contents link */
 	var dest,
 		headerHeight = $( 'div.vectorMenu#usermenu' ).height() + 10,
 		// Split hash
 		hash;
 
-	$( '.toc ul a[href*="#"]' ).click( function( e ) {
+	$( '.toc ul a[href*="#"]' ).click( function ( e ) {
 		if ( this && this.href !== '' ) {
 			// Split location and hash
-			hash = $.escapeSelector( this.href.match( /[#](.*)/ )[ 1 ] ),
+			hash = $.escapeSelector( this.href.match( /[#](.*)/ )[ 1 ] );
 
 			// Don't reload page if already at same location as last clicked
-			$( '#' + hash ).each( function() {
+			$( '#' + hash ).each( function () {
 				$( 'html, body' ).animate( {
 					scrollTop: $( this ).offset().top - headerHeight
 				}, 200 );
@@ -22,7 +22,7 @@ jQuery( document ).ready( function( $ ) {
 	} );
 
 	/* Add offset when scrolling to an anchor present at page load time */
-	$( ':target' ).each( function() {
+	$( ':target' ).each( function () {
 		dest = $( this ).offset().top - headerHeight;
 		$( 'html, body' ).animate( {
 			scrollTop: dest // also use scrollTop to animate scrolling
